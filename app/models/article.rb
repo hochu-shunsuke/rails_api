7f313479f738e76f12c7e17a711592500ce2d388
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-02-24 00:25:06 +0900

    リレーション（1対多）を作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    # dependent: :destroy は親の記事が消えたら紐づくコメントも全部道連れで消す便利設定
+    has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_23_150300) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_23_151539) do
   create_table "articles", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
