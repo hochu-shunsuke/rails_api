7dc39005f45607dcd520bf9fa04fb20ca093903b
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-02-24 00:30:07 +0900

    validationはモデルクラスに書くだけで簡単に実装できる．すげえわ

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
     # dependent: :destroy は親の記事が消えたら紐づくコメントも全部道連れで消す便利設定
     has_many :comments, dependent: :destroy
+
+    # バリデーション
+    validates :title, presence: true, length: { minimum:3, maximum: 50 }
+    validates :content, presence: true
 end
