41eb813a9429916c30c89861e63dfa02d7113a2e
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-02-24 10:14:33 +0900

    comment投稿を可能にした

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+    def create
+        # 1. URLの :article_id を元に、親となる「記事」を探してくる
+        @article = Article.find(params[:article_id])
+
+        # 2. その記事に紐づくコメントを新しく作る（送られてきたパラメータ :content を使う）
+        @comment = @article.comments.create!(content: params[:content])
+        
+        # 3. 成功レスポンスを返す
+        render json:{
+            message: "コメントが正常に作成されました",
+            article_title: @article.title,
+            comment: @comment
+        }, status: :created
+    end
+end
