e936d8910b26a339d01892e49a039986dabe28c3
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-02-24 14:15:42 +0900

    自動生成されたテストコード（Minitest）を認証と引数に合わせて修正

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -3,6 +3,12 @@ require "test_helper"
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @article = articles(:one)
+    @user = users(:one)
+    
+    # 認証用のJWTトークンを生成
+    payload = { user_id: @user.id, exp: 24.hours.from_now.to_i }
+    @token = JWT.encode(payload, Rails.application.secret_key_base)
+    @headers = { "Authorization" => "Bearer #{@token}" }
   end
 
   test "should get index" do
@@ -12,7 +18,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create article" do
     assert_difference("Article.count") do
-      post articles_url, params: { article: { content: @article.content, title: @article.title } }, as: :json
+      post articles_url, params: { article: { content: @article.content, title: @article.title } }, headers: @headers, as: :json
     end
 
     assert_response :created
@@ -24,13 +30,13 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: { content: @article.content, title: @article.title } }, as: :json
+    patch article_url(@article), params: { article: { content: @article.content, title: @article.title } }, headers: @headers, as: :json
     assert_response :success
   end
 
   test "should destroy article" do
     assert_difference("Article.count", -1) do
-      delete article_url(@article), as: :json
+      delete article_url(@article), headers: @headers, as: :json
     end
 
     assert_response :no_content
