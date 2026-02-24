336a025de7f4c9b1a73c834f6a1f984452f4c82e
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-02-24 12:07:14 +0900

    RailsのActionMailerを実装

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome_email.subject
+  #
+
+  default from: 'notifications@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://localhost:3000/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome App')
+  end
+end
